cf245f5b53874966592b2cfbc5bc64efe9fbfdc3
sohammondal <soham.pf.mondal@gmail.com>
2020-02-15 14:17:03 +0530

    handle error if PORT / MONGODB URI missing in .env

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ try {
     mongoose.set('debug', env === "DEV"); // Set Debug mode in DEV 
 
     //Start the server
-    const listener = app.listen(port || 8001, () => {
-        console.log('SERVER LISTENING ON PORT ' + listener.address().port);
-    })
+    if (port) {
+        const listener = app.listen(port, (err) => {
+            console.log('SERVER LISTENING ON PORT ' + listener.address().port);
+        })
+    } else {
+        throw new Error('Pass a free port in the .env file')
+    }
+
 
 } catch (e) {
     console.error('ERROR:', e.message);
+    disconnect();
+    process.exit(1);
 }
